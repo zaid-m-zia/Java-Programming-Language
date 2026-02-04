0c3c1779304aa0eb0ed1f29525f32d2f5563b5b1
Zaid Zia <zaidzia900@gmail.com>
2026-02-04 12:56:24 +0530

    initializing the repo

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("this is a print statement");
+        System.out.println("this is an another print statement ");
+    }
+}
+
+
